0d2ef67157065640cd5d96d79055b489caea39cf
Donghun <easternhalo@mju.ac.kr>
2022-11-20 22:12:11 +0900

    SocketClient, SocketServerSide 작성

--- project/backend/SocketClient.c
+++ project/backend/SocketClient.c
@@ -12,4 +12,32 @@ int main(int argc, char const* argv[])
     struct sockaddr_in server_address;
     char* message = "Message from client please respond";
     char buffer[1024] =  {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+         printf("\n Error while creation of Socket \n");
+         return -1;
+    }
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons (PORT) ;
+
+    //IPv4와 IPv6 주소를 문자에서 이진수로 변환 
+    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
+         printf("\nInvalid server address/ Address not found \n");
+         return -1;
+     }
+
+     //connect 명령어로 소켓 서버로 연결함
+    if ((client_file_descriptor= connect(sock, (struct sockaddr*)&server_address, sizeof (server_address)))< 0) {
+        printf("\nConnection Failed with the server \n");
+        return -1;
+    }
+
+    //send 명령어로 메세지를 소켓 서버로 보냄 
+    send (sock, message, strlen (message), 0);
+    printf("Message from Client Successfully Sent\n");
+    value_Read = read(sock, buffer, 1024);
+    printf("Response from Server: %s\n", buffer);
+
+    //연결된 소켓 닫음
+    close(client_file_descriptor);
+    return 0;
 }
--- project/backend/SocketServerSide.c
+++ project/backend/SocketServerSide.c
@@ -4,14 +4,14 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-
+#define PORT 8080
 
 int main(int argc, char const* argv[])
 {
     int server_file_desc, new_socket, value_Read;
     struct sockaddr_in socket_Address;
     int option = 1;
-    int addres_Length = sizeof(socket_Address);
+    int address_Length = sizeof(socket_Address);
     char buffer[1024] = {0};
     char* message = "Greetings Message from Server";
 
@@ -48,14 +48,20 @@ int main(int argc, char const* argv[])
         exit(EXIT_FAILURE);
     }
 
-    if((new_socket = accept(server_file_desc,(struct sockaddr*)&socket_Address,))
-
-
-
-
-
-
-
-
+    if((new_socket = accept(server_file_desc,(struct sockaddr*)&socket_Address,(socklen_t*)&address_Length)) <0){
+        perror ("Connect is not Accepted."); exit (EXIT_FAILURE);
+    }
     
+    //서버에서 보낸 메세지 읽음
+    value_Read = read(new_socket, buffer, 1024);
+    printf(" Message from Client: %s\n", buffer);
+    send (new_socket, message, strlen (message), 0);
+    printf("Message from Server is Sent to client\n");
+
+    //연결된 소켓 닫음
+    close (new_socket);
+    
+    //듣는 소켓 닫음
+    shutdown(server_file_desc, SHUT_RDWR);
+    return 0;
 }
