22820921226a87755e2fb8da6f1f87e6999b6e44
kimseonghyeon <dhkdwk0215@naver.com>
2022-11-22 17:20:07 +0900

    hello drv update

--- hw/hw2/hello_drv.c
+++ hw/hw2/hello_drv.c
@@ -1,29 +1,26 @@
-// 모듈형 문자 디바이스 드라이버(hello_drv.c) 및
-
-//  Makefile 작성
-//  Compile(make명령) 후, Target으로 ‘hello_drv.ko’ 를 전송 (scp, minicom, whatever)
-//  키트에서
-//  디바이스 노드 생성 (mknod)
-//  디바이스 드라이브 적재 (insmod)
-//  PC에서
-//  응용프로그램 작성 (hello_test.c)
-//  응용프로그램 컴파일 후, 실행 파일 (hello_test)을 타겟으로 전송
-//  응용 프로그램 실행 (./hello_test
-
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <asm/uaccess.h>
 
+int init_hello(void);
+void exit_hello(void);
+
+static int hello_open(struct inode *, struct file *);
+static int hello_close(struct inode *, struct file *);
+static ssize_t hello_read(struct file *, char __user *, size_t , loff_t *);
+static ssize_t hello_write(struct file *, const char *, size_t, loff_t *);
+static loff_t hello_lseek(struct file *file, loff_t offset, int orig);
+
 #define HELLO_MAJOR_NUM 290
 #define HELLO_NAME "hello"
-
-static char array[2000]; // Global 변수, Device Driver에서 사용
+#define BUFFER_SIZE 2000
+static char array[BUFFER_SIZE]; // Global 변수, Device Driver에서 사용
 
 static int hello_ioctl(struct file *flip, unsigned int cmd, unsigned long arg) // from K.2.6.36
 {
-    int cnt = 0;
+    
     int k = 0;
     if (_IOC_TYPE(cmd) != 0x55)
     {
@@ -32,18 +29,21 @@ static int hello_ioctl(struct file *flip, unsigned int cmd, unsigned long arg) /
     }
     switch (_IOC_NR(cmd))
     {
+
+        //그외 에러처리 (ex, *filepos > 2000 ) 등 처리를 해 줘야 함.
+
     case 99:
-        printk("->% d\n", _IOC_SIZE(cmd)); // should be 20
-        copy_from_user(array, arg, _IOC_SIZE(cmd));
+        printk("\tIOCTL Size: %d\n", _IOC_SIZE(cmd)); // should be 8
+        copy_from_user(&array, arg, _IOC_SIZE(cmd));
         // do something.
 
         printk("99번 작동");
 
         break;
     case 98:
-        printk("->% d\n", _IOC_SIZE(cmd)); // should be 4
-        copy_from_user(&cnt, arg, _IOC_SIZE(cmd));
-        for (k = 0; k < cnt; k++)
+        printk("\tIOCTL Size: %d\n", _IOC_SIZE(cmd)); // should be 8
+        copy_from_user(&array, arg, _IOC_SIZE(cmd));
+        for (k = array[0]; k < (array[0] + array[1]); k++)
             printk("%c", array[k]);
         printk("\n");
         break;
@@ -59,29 +59,18 @@ static int hello_open(struct inode *node, struct file *pfile)
 
 static ssize_t hello_read(struct file *pfile, char __user *pBuff, size_t size, loff_t *filepos)
 {
-
     printk("hello_read enter\n");
-    if (size >= 4)
-    {
-        copy_to_user(pBuff, array, 2000);
-        // put_user (i, pBuff); //i 변수값을 pBuff 사용자 메모리값에 대입
-        return 4;
-    }
-    // pfile->f_flags 를 읽어보면 open시 줬던 옵션값을 읽힘: O_NONBLOCK 등
-    return 0;
+    copy_to_user(pBuff, &array[*filepos], size);
+    *filepos += size;
+    return size;
 }
 
 static ssize_t hello_write(struct file *pfile, const char __user *pBuff, size_t size, loff_t *filepos)
 {
-    // char arrData[4];
     printk("hello_write enter\n");
-    if (size >= 4)
-    {
-        copy_from_user(array, pBuff, 2000);
-        // get_user ( i, pBuff ); //i변수에 pBuff의 값을 대입한다.
-        return 4;
-    }
-    return 0;
+    copy_from_user(&array[*filepos], pBuff, size);
+    *filepos += size;
+    return size;
 }
 
 static int hello_release(struct inode *node, struct file *pfile)
@@ -90,7 +79,6 @@ static int hello_release(struct inode *node, struct file *pfile)
     return 0;
 }
 
-
 static const struct file_operations hello_fops = {
     .owner = THIS_MODULE,
     .open = hello_open,
@@ -98,7 +86,7 @@ static const struct file_operations hello_fops = {
     .write = hello_write,
     .release = hello_release,
     .unlocked_ioctl = hello_ioctl,
-    //.llseek = hello_llseek,
+    .llseek = hello_lseek,
 };
 
 int flagRegisterSuccess = 0;
@@ -127,20 +115,31 @@ void __exit exit_hello(void)
     printk("Good-bye~ \n");
 }
 
-// static loff_t hello_llseek(struct file *file, loff_t offset, int orig)
-// { // file read/write pointer: file->f_pos
-//     loff_t newPos = 0;
-//     if (orig == 0)
-//         newPos = offset; // SEEK_SET
-//     else if (orig == 1)
-//         newPos = file->f_pos + offset; // SEEK_CUR
-//     else
-//         newPos = 2000 - offset; // SEEK_END, 내부 버퍼 사이즈=2000
-//     file->f_pos = newPos;
-//     printk(“New File R / W Ptr
-//            : % d\n”, (int)file->f_pos);
-//     return file->f_pos;
-// }
+static loff_t hello_lseek(struct file *file, loff_t offset, int orig)
+{ // file read/write pointer: file->f_pos
+    loff_t newPos = 0;
+    printk("hello lseek working\n");
+    if (orig == 0)
+    {
+        newPos = offset; // SEEK_SET
+    }
+    else if (orig == 1)
+    {
+        newPos = file->f_pos + offset; // SEEK_CUR
+    }
+    else
+    {
+        newPos = BUFFER_SIZE - offset; // SEEK_END, 내부 버퍼 사이즈=2000
+    }
+
+    if (newPos > BUFFER_SIZE)
+        newPos = BUFFER_SIZE;
+    if (newPos < 0)
+        newPos = 0;
+    file->f_pos = newPos;
+    printk("New File R / W Ptr %d\n", (int)file->f_pos);
+    return file->f_pos;
+}
 
 module_init(init_hello); //  모듈 로딩시(insmod) 해당 함수 호출
 module_exit(exit_hello); //  모듈 제거시(rmmod) 해당 함수 호출
