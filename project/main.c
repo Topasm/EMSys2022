76b2538c7c267332dcd8e5603a21c6492336f5db
kimseonghyeon <dhkdwk0215@naver.com>
2022-11-28 09:18:00 +0900

    add main

---
+++ project/main.c
@@ -0,0 +1,14 @@
+//최종 프로젝트 파일
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "driver.h"
+
