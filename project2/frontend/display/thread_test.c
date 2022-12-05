980a2784a64cb4186a08dd020a15d07f770bf508
soomin-park <1127allison@gmail.com>
2022-12-05 17:41:01 +0900

    relocate

--- project2/frontend/display/thread_test.c
+++ project2/frontend/display/thread_test.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <pthread.h>
-#include "frontend/libfbdev/libfbdev.h"
-#include "frontend/lodepng/lodepng.h"
-#include "device/gyro.h"
-#include "frontend/frontend.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../libfbdev/libfbdev.h"
+#include "../lodepng/lodepng.h"
+#include "../../device/gyro.h"
+
+char *bg_data;
+char *mari_data;
+char *maru_data;
+char *menu_data;
+
+unsigned int error;
+const char *bg_path = "../source/background.png";
+const char *mari_path = "../source/mari.png";
+const char *maru_path = "../source/mari.png"; //maru 로 변경 해야 함
+const char *menu_path = "../source/menu.png";
+
+int cols_bg = 0, rows_bg = 0;
+int cols_mari = 0, rows_mari = 0;
+int cols_maru = 0, rows_maru = 0;
+int cols_menu = 0, rows_menu = 0;
 
 
-//int x=100; 
-//int flag_ForBack=1;
-
-/* void png_init()
+void png_init()
 {
 	error = lodepng_decode32_file(&bg_data, &cols_bg, &rows_bg, bg_path);
 	if (error)
@@ -24,13 +37,15 @@
 	error = lodepng_decode32_file(&maru_data, &cols_maru, &rows_maru, maru_path);
 	if (error)
 		printf("error %u: %s\n", error, lodepng_error_text(error));
+	error = lodepng_decode32_file(&menu_data, &cols_menu, &rows_menu, menu_path);
+	if (error)
+		printf("error %u: %s\n", error, lodepng_error_text(error));
 }
 
 void update_background(void)
 {
 	picture_in_position(bg_data, cols_bg, rows_bg,0,0);
 }
-
 void update_mari(int posx, int posy)
 {
 	picture_in_position(mari_data, cols_mari, rows_mari,posx,posy);
@@ -38,8 +53,15 @@ void update_mari(int posx, int posy)
 void update_maru(int posx, int posy)
 {
 	picture_in_position(maru_data, cols_maru, rows_maru,posx,posy);
-} */
+}
+void update_menu(void)
+{
+	picture_in_position(menu_data, cols_menu, rows_menu,0,0);
+}
 
+
+//player 변수
+int player = 1; //1p or 2p
 
 int move_left(void)
 {	
@@ -52,6 +74,7 @@ int move_left(void)
 		update_mari(x1,200);
 		update_screen();
 
+        if
 		if(x1>=240)	
 		{
 			printf("saturation\n");
@@ -70,7 +93,25 @@ int move_left(void)
 			else
 				x1=0;
 		}
-		else
+        else if(x2<=300)
+        {
+            printf("maru saturation\n");
+            dx2=get_dx();
+            if(dx2>0)
+                x2=x2-dx2;
+            else
+                dx=300;
+        }
+        else if(x2>=550)
+        {
+            printf("maru saturation\n");
+            dx1=get_dx();
+			if(dx2<0)
+				x2=x2-dx2;
+			else
+				x2=0;
+        }
+		else if(x1)
 		{
 			printf("%d\n",x1);
 			dx1=get_dx();
@@ -81,3 +122,6 @@ int move_left(void)
 	fb_close();
 	return 0;
 }
+
+
+
