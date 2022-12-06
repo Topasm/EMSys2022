30b2e283ae7206838bc15f71c8f8c7c6f5f9a836
Donghun <easternhalo@mju.ac.kr>
2022-12-06 16:52:05 +0900

    modify server.c, client.c

--- project/device/Client.c
+++ project/device/Client.c
@@ -3,41 +3,50 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdlib.h>
 #define PORT 8080
 
- int main(int arg, char const* argv[])
+int main(int arg, char const *argv[])
 {
-int sock = 0, value_Read, client_file_descriptor;
-struct sockaddr_in server_address;
-// char* message ="Hello, Message from Client, Please Response"; //서버로 보내는 메세지  
-char* message ="클라이언트에서 보내는 x, y좌표"; //서버로 보내는 메세지  
-char buffer [1024] = { 0 };
-if ((sock = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
-printf("In Error while creation of Socket \n");
-return -1;
-}
+     int mari_x, maru_x, ball_x;
+    int sock = 0, value_Read, client_file_descriptor;
+    struct sockaddr_in server_address;
+    char *message = "클라이언트에서 보내는 x, y좌표"; //서버로 보내는 메세지
+    char strCmd[100];
+    char buffer[100] = {0};
+    int result;
+    sprintf(strCmd, "./게임 '%d, %d, %d'", mari_x, maru_x, ball_x);
+    result = system(strCmd); //문자열 그대로 실행시키는 함수
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("In Error while creation of Socket \n");
+        return -1;
+    }
 
-server_address.sin_family = AF_INET;
-server_address.sin_port = htons(PORT);
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
 
-// Converting the IPv4 and IPv6 addresses from text to binary format 
-if (inet_pton(AF_INET, "192.168.108.15", &server_address.sin_addr) <= 0) {
- printf("\nInvalid server address/ Address not found \n");
-return -1;
-}
-//Going to connet to the socket server using connect method 
-if ((client_file_descriptor = connect(sock, (struct sockaddr*)&server_address, sizeof (server_address))) < 0) {
-printf("\nConnection Failed with the server \n");
-return -1;
-}
-//send message to socket server using send method 
-send (sock, message, strlen(message), 0);
-printf("Message from Client Successfully Sent\n");
-while(1){
-value_Read = read(sock, buffer, 1024);
-printf("Response from Server: %s\n", buffer);
-}
-// closing the connected socket
-close(client_file_descriptor);
-return 0;
+    // Converting the IPv4 and IPv6 addresses from text to binary format
+    if (inet_pton(AF_INET, "192.168.108.15", &server_address.sin_addr) <= 0)
+    {
+        printf("\nInvalid server address/ Address not found \n");
+        return -1;
+    }
+    // Going to connet to the socket server using connect method
+    if ((client_file_descriptor = connect(sock, (struct sockaddr *)&server_address, sizeof(server_address))) < 0)
+    {
+        printf("\nConnection Failed with the server \n");
+        return -1;
+    }
+    // send message to socket server using send method
+    send(sock, strCmd, strlen(strCmd), 0);
+    printf("Message from Client Successfully Sent\n");
+    while (1)
+    {
+        value_Read = read(sock, strCmd, 100);
+        printf("Response from Server: %s\n", strCmd);
+    }
+    // closing the connected socket
+    close(client_file_descriptor);
+    return 0;
 }
--- project/device/Server.c
+++ project/device/Server.c
@@ -1,67 +1,72 @@
- #include <netinet/in.h>
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
- #include <sys/socket.h>
- #include <unistd.h>
- #define PORT 8080
- int main(int arg, char const* argv[])
- {
-int server_file_desc, new_socket, value_Read;
-struct sockaddr_in socket_Address;
-int option = 1;
-int address_Length = sizeof(socket_Address);
-char buffer [1024] = { 0 };
-// char* message = "Greetings Message from Server!!!"; //클라이언트로 보내는 거
-char* message = "서버에서 보내는 x, y좌표"; //클라이언트로 보내는 거
-// Creating socket file descriptor
-if ((server_file_desc = socket (AF_INET, SOCK_STREAM, 0)) == 0) 
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+int main(int arg, char const *argv[])
 {
-perror ("Failed to Obtained Socket File Descriptor");
-exit (EXIT_FAILURE);
-}
+    int mari_x, maru_x, ball_x;
+    int server_file_desc, new_socket, value_Read;
+    struct sockaddr_in socket_Address;
+    int option = 1;
+    int address_Length = sizeof(socket_Address);
+    char strCmd[100];
+    char buffer[100] = {0};
+    int result;
+    sprintf(strCmd, "./게임 '%d, %d, %d'", mari_x, maru_x, ball_x);
+    result = system(strCmd); //문자열 그대로 실행시키는 함수
 
-// Forcefully attaching socket to the port 8080 
-if (setsockopt (server_file_desc, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option, sizeof (option))) {
-perror ("Setting Socket Failed.");
-exit(EXIT_FAILURE);
-}
-socket_Address.sin_family = AF_INET;
-socket_Address.sin_addr.s_addr = INADDR_ANY;
-socket_Address.sin_port = htons (PORT);
-// Forcefully attaching socket to the port 8080 
-if (bind(server_file_desc, (struct sockaddr*)&socket_Address,
-sizeof (socket_Address)) < 0) {
-perror ("Bind File Descriptor on Port 8080 Failed");
-exit (EXIT_FAILURE);
-}
-printf ("Server is Waiting for Client Connection! \n");
-if (listen (server_file_desc, 3) < 0) {
-perror ("Listening Failed.");
-exit(EXIT_FAILURE);
-}
-if ((new_socket = accept(server_file_desc, (struct sockaddr*)&socket_Address, (socklen_t*)&address_Length)) < 0) {
-perror ("Connect is not Accepted.");
-exit(EXIT_FAILURE);
-}
+    // Creating socket file descriptor
+    if ((server_file_desc = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("Failed to Obtained Socket File Descriptor");
+        exit(EXIT_FAILURE);
+    }
 
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_file_desc, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option, sizeof(option)))
+    {
+        perror("Setting Socket Failed.");
+        exit(EXIT_FAILURE);
+    }
+    socket_Address.sin_family = AF_INET;
+    socket_Address.sin_addr.s_addr = INADDR_ANY;
+    socket_Address.sin_port = htons(PORT);
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_file_desc, (struct sockaddr *)&socket_Address,
+             sizeof(socket_Address)) < 0)
+    {
+        perror("Bind File Descriptor on Port 8080 Failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Server is Waiting for Client Connection! \n");
+    if (listen(server_file_desc, 3) < 0)
+    {
+        perror("Listening Failed.");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_file_desc, (struct sockaddr *)&socket_Address, (socklen_t *)&address_Length)) < 0)
+    {
+        perror("Connect is not Accepted.");
+        exit(EXIT_FAILURE);
+    }
 
-//Reading the Message sent from Server
-value_Read = read(new_socket, buffer, 1024);
-// printf ("Message from Client: %s \n", buffer); //버퍼에 클라이언트 메세지 쓰여있음
+    // Reading the Message sent from Server
+    value_Read = read(new_socket, buffer, 100);
+    // printf ("Message from Client: %s \n", buffer); //버퍼에 클라이언트 메세지 쓰여있음
 
+    while (1)
+    {
+        printf("Message from Client: %s \n", buffer); //버퍼에 클라이언트 메세지 쓰여있음
 
-// int i = 0;
-
-// for(i=0;i < 10; i++){
-while(1){
-printf ("Message from Client: %s \n", buffer); //버퍼에 클라이언트 메세지 쓰여있음
-
-send (new_socket, message, strlen(message), 0); 
-printf ("Message from Server is Sent to client\n");
-}// closing the connected socket
-close(new_socket);
-// closing the listening socket
-shutdown (server_file_desc, SHUT_RDWR);
-return 0;
+        send(new_socket, strCmd, strlen(strCmd), 0);
+        printf("Message from Server is Sent to client\n");
+    } // closing the connected socket
+    close(new_socket);
+    // closing the listening socket
+    shutdown(server_file_desc, SHUT_RDWR);
+    return 0;
 }
